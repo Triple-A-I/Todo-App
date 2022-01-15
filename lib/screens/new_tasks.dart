281296d29f57ec0b6e840eab06ec7d8d15aa85f6
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-01-15 11:52:32 -0800

    build new tasks and feed it with data from database

--- lib/screens/new_tasks.dart
+++ lib/screens/new_tasks.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/constants.dart';
+import 'package:recipes/models/task.dart';
+import 'package:recipes/screens/widgets/task_item.dart';
 
 class NewTasksScreen extends StatelessWidget {
-  const NewTasksScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'New Tasks',
-        style: TextStyle(
-          fontSize: 25,
-        ),
-      ),
+    return ListView.separated(
+      itemBuilder: (context, index) =>
+          buildTaskItem(Task.fromJson(tasks[index])),
+      separatorBuilder: (context, index) => Divider(color: Colors.grey[400]),
+      itemCount: tasks.length,
     );
   }
 }
