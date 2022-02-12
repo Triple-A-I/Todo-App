08f93f6f6dd42305bbf779a31276596c605cf22c
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 15:04:56 -0800

    create show snackbar function

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+SnackBar showSnackBar(BuildContext context, String message) {
+  return SnackBar(
+      content: Text(message),
+      backgroundColor: (Colors.black12),
+      action: SnackBarAction(
+        label: 'dismiss',
+        onPressed: () {},
+      ));
+}
