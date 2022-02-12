0b7a0ff2cc3dbe112e43e5dfc896e7b513426440
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 01:42:11 -0800

    Add createDatabase, changeBottomSheetState, insertIntoDatabase, getDataFromDataFromDatabase Functions to the cubit

--- lib/business_logic/cubit/app_cubit.dart
+++ lib/business_logic/cubit/app_cubit.dart
@@ -6,6 +6,7 @@ import 'package:meta/meta.dart';
 import 'package:recipes/screens/archived_tasks_screen.dart';
 import 'package:recipes/screens/done_tasks_screen.dart';
 import 'package:recipes/screens/new_tasks.dart';
+import 'package:sqflite/sqflite.dart';
 
 part 'app_state.dart';
 
@@ -21,12 +22,73 @@ class AppCubit extends Cubit<AppStates> {
     const ArchivedTasksScreen(),
   ];
 
-  static AppCubit get(BuildContext context) =>
-      BlocProvider.of<AppCubit>(context);
+  static AppCubit get(BuildContext context) => BlocProvider.of(context);
 
   void changeIndex(int index) {
     currentIndex = index;
 
     emit(AppChangeBottomNavBarState());
+  }
+
+  void createDatabase(BuildContext context) {
+    openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version) {
+        database
+            .execute(
+                "CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)")
+            .then((value) {})
+            .catchError((error) {
+          showDialog(
+              context: context, builder: (context) => Text(error.toString()));
+        });
+      },
+      onOpen: (database) {
+        getDataFromDataFromDatabase(database).then((value) {
+          tasks = value;
+          print(tasks);
+          emit(AppGetDatabaseState());
+        });
+      },
+    ).then((value) {
+      database = value;
+      emit(AppCreateDatabaseState());
+    });
+  }
+
+  IconData fabIcon = Icons.edit;
+  bool isBottomSheetShowing = false;
+
+  void changeBottomSheetState({required IconData icon, required bool isShow}) {
+    isBottomSheetShowing = isShow;
+    fabIcon = icon;
+    emit(AppChangeBottomSheetState());
+  }
+
+  late Database database;
+  List<Map> tasks = [];
+
+  Future<void> insertIntoDatabase(
+      String title, String time, String date) async {
+    await database.transaction((txn) => txn
+            .rawInsert(
+                "INSERT INTO tasks(title, date, time, status) VALUES('$title','$date','$time','new')")
+            .then((value) {
+          print('$value inserted to Database Successfully');
+          emit(AppInsertIntoDatabaseState());
+          getDataFromDataFromDatabase(database).then((value) {
+            tasks = value;
+            print(tasks);
+            emit(AppGetDatabaseState());
+          });
+        }).catchError(
+          (error) {},
+        ));
+  }
+
+  Future<List<Map>> getDataFromDataFromDatabase(database) async {
+    emit(AppGetDatabaseLoadingState());
+    return await database.rawQuery("SELECT * FROM tasks");
   }
 }
