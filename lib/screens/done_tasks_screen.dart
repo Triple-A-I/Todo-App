e5bd5dccfb6ce83dbbdee366767e36fa5ca8d39d
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 15:05:51 -0800

    create done tasks screen

--- lib/screens/done_tasks_screen.dart
+++ lib/screens/done_tasks_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/business_logic/cubit/app_cubit.dart';
+import 'package:todo_app/models/task.dart';
+import 'package:todo_app/widgets/task_item.dart';
 
 class DoneTasksScreen extends StatelessWidget {
   const DoneTasksScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Done Tasks',
-        style: TextStyle(
-          fontSize: 25,
-        ),
-      ),
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var tasks = AppCubit.get(context).doneTasks;
+        return ListView.separated(
+          itemBuilder: (context, index) {
+            if (tasks.isEmpty) {
+              return const Center(
+                child: Text(
+                  'No Tasks To Show',
+                  style: TextStyle(color: Colors.black),
+                ),
+              );
+            } else {
+              return buildTaskItem(Task.fromJson(tasks[index]), context);
+            }
+          },
+          separatorBuilder: (context, index) =>
+              Divider(color: Colors.grey[400]),
+          itemCount: tasks.length,
+        );
+      },
     );
   }
 }
