bc60b103b24658ce238cb2ac136012dd5bf1be20
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 01:44:03 -0800

    add AppCreateDatabaseState, AppGetDatabaseState, AppGeteDatabaseLoadingState, AppInsertIntoDatabaseState, AppChangeBottomSheetState

--- lib/business_logic/cubit/app_state.dart
+++ lib/business_logic/cubit/app_state.dart
@@ -6,3 +6,13 @@ abstract class AppStates {}
 class AppInitialState extends AppStates {}
 
 class AppChangeBottomNavBarState extends AppStates {}
+
+class AppCreateDatabaseState extends AppStates {}
+
+class AppGetDatabaseState extends AppStates {}
+
+class AppGetDatabaseLoadingState extends AppStates {}
+
+class AppInsertIntoDatabaseState extends AppStates {}
+
+class AppChangeBottomSheetState extends AppStates {}
