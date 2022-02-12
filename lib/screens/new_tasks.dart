0f3ba68178242068be6ef538a86e753a99effac6
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 11:57:01 -0800

    Wrap item with Slidable Widget

--- lib/screens/new_tasks.dart
+++ lib/screens/new_tasks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:recipes/business_logic/cubit/app_cubit.dart';
 import 'package:recipes/models/task.dart';
 import 'package:recipes/widgets/task_item.dart';
@@ -13,8 +14,56 @@ class NewTasksScreen extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         return ListView.separated(
-          itemBuilder: (context, index) =>
-              buildTaskItem(Task.fromJson(AppCubit.get(context).tasks[index])),
+          itemBuilder: (context, index) => Slidable(
+            // Specify a key if the Slidable is dismissible.
+            key: ObjectKey(index),
+
+            // The start action pane is the one at the left or the top side.
+            endActionPane: ActionPane(
+              // A motion is a widget used to control how the pane animates.
+              motion: const BehindMotion(),
+
+              // A pane can dismiss the Slidable.
+              dismissible: DismissiblePane(
+                onDismissed: () {
+                  print('Albraa Done');
+                },
+              ),
+
+              // All actions are defined in the children parameter.
+              children: [
+                // A SlidableAction can have an icon and/or a label.
+                SlidableAction(
+                  onPressed: (BuildContext context) {
+                    print('Albraa Done');
+                  },
+                  backgroundColor: const Color(0xFF00FF9a),
+                  foregroundColor: Colors.white,
+                  icon: Icons.done,
+                  label: 'Done',
+                ),
+              ],
+            ),
+
+            // The end action pane is the one at the right or the bottom side.
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  // An action can be bigger than the others.
+                  flex: 2,
+                  onPressed: (BuildContext context) {},
+                  backgroundColor: Color(0xFFFF0000),
+                  foregroundColor: Colors.white,
+                  icon: Icons.archive,
+                  label: 'Archive',
+                ),
+              ],
+            ),
+
+            child: buildTaskItem(
+                Task.fromJson(AppCubit.get(context).tasks[index])),
+          ),
           separatorBuilder: (context, index) =>
               Divider(color: Colors.grey[400]),
           itemCount: AppCubit.get(context).tasks.length,
