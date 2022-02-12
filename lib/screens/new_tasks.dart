89683e363ffa2e7314db497ab21ec51bf580c648
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-12 15:05:25 -0800

    create new tasks screen

--- lib/screens/new_tasks.dart
+++ lib/screens/new_tasks.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:todo_app/business_logic/cubit/app_cubit.dart';
 import 'package:todo_app/models/task.dart';
 import 'package:todo_app/widgets/task_item.dart';
@@ -13,60 +12,23 @@ class NewTasksScreen extends StatelessWidget {
     return BlocConsumer<AppCubit, AppStates>(
       listener: (context, state) {},
       builder: (context, state) {
+        var tasks = AppCubit.get(context).newTasks;
         return ListView.separated(
-          itemBuilder: (context, index) => Slidable(
-            // Specify a key if the Slidable is dismissible.
-            key: ObjectKey(index),
-
-            // The start action pane is the one at the left or the top side.
-            endActionPane: ActionPane(
-              // A motion is a widget used to control how the pane animates.
-              motion: const BehindMotion(),
-
-              // A pane can dismiss the Slidable.
-              dismissible: DismissiblePane(
-                onDismissed: () {
-                  print('Albraa Done');
-                },
-              ),
-
-              // All actions are defined in the children parameter.
-              children: [
-                // A SlidableAction can have an icon and/or a label.
-                SlidableAction(
-                  onPressed: (BuildContext context) {
-                    print('Albraa Done');
-                  },
-                  backgroundColor: const Color(0xFF00FF9a),
-                  foregroundColor: Colors.white,
-                  icon: Icons.done,
-                  label: 'Done',
+          itemBuilder: (context, index) {
+            if (tasks.isEmpty) {
+              return const Center(
+                child: Text(
+                  'No Tasks To Show',
+                  style: TextStyle(color: Colors.black),
                 ),
-              ],
-            ),
-
-            // The end action pane is the one at the right or the bottom side.
-            startActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              children: [
-                SlidableAction(
-                  // An action can be bigger than the others.
-                  flex: 2,
-                  onPressed: (BuildContext context) {},
-                  backgroundColor: Color(0xFFFF0000),
-                  foregroundColor: Colors.white,
-                  icon: Icons.archive,
-                  label: 'Archive',
-                ),
-              ],
-            ),
-
-            child: buildTaskItem(
-                Task.fromJson(AppCubit.get(context).tasks[index])),
-          ),
+              );
+            } else {
+              return buildTaskItem(Task.fromJson(tasks[index]), context);
+            }
+          },
           separatorBuilder: (context, index) =>
               Divider(color: Colors.grey[400]),
-          itemCount: AppCubit.get(context).tasks.length,
+          itemCount: tasks.length,
         );
       },
     );
