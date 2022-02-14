d7e5bd9692058fe40a1a7263ba5ffff88b29a585
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-14 05:32:14 -0800

    change duration of Snackbar

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 SnackBar showSnackBar(BuildContext context, String message) {
   return SnackBar(
+    duration: const Duration(milliseconds: 500),
     content: Text(
       message,
       style: const TextStyle(
