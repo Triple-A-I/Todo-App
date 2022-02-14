a049dc68f806b5ee05d01653d6f0c7a1df7c1b5d
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-14 03:48:28 -0800

    Ignore immutable class warning

--- lib/business_logic/cubit/app_state.dart
+++ lib/business_logic/cubit/app_state.dart
@@ -13,6 +13,7 @@ class AppGetDatabaseState extends AppStates {}
 
 class AppGetDatabaseLoadingState extends AppStates {}
 
+// ignore: must_be_immutable
 class AppInsertIntoDatabaseState extends AppStates {
   int id;
   AppInsertIntoDatabaseState(this.id);
@@ -20,14 +21,20 @@ class AppInsertIntoDatabaseState extends AppStates {
 
 class AppChangeBottomSheetState extends AppStates {}
 
+// ignore: must_be_immutable
 class AppArchiveTaskState extends AppStates {
   int id;
   AppArchiveTaskState(this.id);
 }
 
+// ignore: must_be_immutable
 class AppADoneTaskState extends AppStates {
   int id;
   AppADoneTaskState(this.id);
 }
 
-class AppADeleteTaskState extends AppStates {}
+// ignore: must_be_immutable
+class AppADeleteTaskState extends AppStates {
+  int id;
+  AppADeleteTaskState(this.id);
+}
